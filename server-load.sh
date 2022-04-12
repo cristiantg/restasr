098699b5b024b2c3b4c3cad652fa530c451b8cd4
Cristian Tejedor García <cristian.tejedor.91@gmail.com>
2022-04-12 12:10:04 +0200

    Added beam parameter

--- server-load.sh
+++ server-load.sh
@@ -2,4 +2,4 @@
 
 # Make sure your audio file is correctly encoded:
 #sox . source/henk/henk.wav -r 16000 -c 1 -b 16 raw_data/henk/henk_conv.wav
-./uber_single.sh . raw_data/henk henk.wav output/henk henk speaker > logfile 2>&1
+./uber_single.sh . raw_data/henk henk.wav output/henk henk speaker 15 > logfile 2>&1
